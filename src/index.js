939ab3efe6cb48cbd1a2685d9c567eb84ab26ee8
AnasNajjar1 <anas.najjar@sesame.com.tn>
2021-05-30 20:55:46 +0200

    added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const productRouter = require('./routers/product.js');
 const userRouter = require('./routers/user.js');
@@ -11,6 +12,7 @@ const uploadRouter = require('./routers/upload.js');
 
 const app = express();
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
