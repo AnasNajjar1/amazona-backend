5c04b97a9909d9a369feb0ff96336075e26ba61a
Anas Najjar <anas.najjar@sesame.com.tn>
2021-06-13 01:10:48 +0200

    trying on sellers

--- src/routers/order.js
+++ src/routers/order.js
@@ -8,10 +8,24 @@ const orderRouter = express.Router();
 orderRouter.get('/', isAuth, isSellerOrAdmin,  expressAsyncHandler(async (req, res) => {
 
     const seller = req.query.seller || '';
-    const sellerFilter = seller? { seller } : {};
+    // const sellerFilter = seller? { seller } : {};
 
-    const orders = await Order.find({...sellerFilter}).populate('user', 'name');
-    res.send(orders);
+    const orders = await Order.find({}).populate('user', 'name');
+    
+    if(seller != '') {
+        let ordersSeller = [];
+        for(let order of orders) {
+            let validOrder = order.sellers.find(s => s == seller);
+            if(validOrder) {
+                ordersSeller.push(order);
+            }
+        }
+        res.send(ordersSeller);
+    } else {
+        res.send(orders);
+    }
+   
+   
 }));
 
 orderRouter.get('/mine', isAuth, expressAsyncHandler(async (req, res) => {
